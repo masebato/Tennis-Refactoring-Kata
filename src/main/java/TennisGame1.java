d7af62c0f859dfcab735a7bca04dbc084ae8d0a9
masebato <baradona69@gmail.com>
2018-10-12 16:39:50 -0500

    solucion

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,10 +1,12 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_puntaje1 = 0;
-    private int m_puntaje2 = 0;
+    private int PuntajeJugador1 = 0;
+    private int PuntajeJugador2 = 0;
     private String nombreJugador1;
     private String nombreJugador2;
+    private String puntaje="";
+    private int PuntajeTemporal=0;
 
     public TennisGame1(String nombreJugador1, String nombreJugador2) {
         this.nombreJugador1 = nombreJugador1;
@@ -13,62 +15,93 @@ public class TennisGame1 implements TennisGame {
 
     public void ganoPunto(String nombreJugador) {
         if (nombreJugador == "jugador1")
-            m_puntaje1 += 1;
+            PuntajeJugador1 += 1;
         else
-            m_puntaje2 += 1;
+            PuntajeJugador2 += 1;
     }
 
     public String getPuntaje() {
-        String puntaje = "";
-        int tempPuntaje=0;
-        if (m_puntaje1 == m_puntaje2)
+       
+     
+        if (PuntajeJugador1 == PuntajeJugador2)
         {
-            switch (m_puntaje1)
-            {
-                case 0:
-                        puntaje = "Amor-Todos";
-                    break;
-                case 1:
-                        puntaje = "Quince-Todos";
-                    break;
-                case 2:
-                        puntaje = "Treinta-Todos";
-                    break;
-                default:
-                        puntaje = "Deuce";
-                    break;
-                
-            }
+        	return GetPuntajeEmpate(PuntajeJugador1);
         }
-        else if (m_puntaje1 >=4 || m_puntaje2 >=4)
+        else if (PuntajeJugador1 >=4 || PuntajeJugador2 >=4)
         {
-            int minusResult = m_puntaje1 - m_puntaje2;
-            if (minusResult==1) puntaje ="Ventaja jugador1";
-            else if (minusResult ==-1) puntaje ="Ventaja jugador2";
-            else if (minusResult>=2) puntaje = "Gana jugador1";
-            else puntaje ="Gana jugador2";
+        	return  GetEstadoMarcador();
         }
         else
         {
-            for (int i=1; i<3; i++)
+        	return GetResultados() ;
+        }
+       
+    }
+    
+    
+    private String GetPuntajeEmpate(int PuntajeJugador) {
+    	
+        switch (PuntajeJugador)
+        {
+            case 0:
+                    puntaje = "Amor-Todos";
+                break;
+            case 1:
+                    puntaje = "Quince-Todos";
+                break;
+            case 2:
+                    puntaje = "Treinta-Todos";
+                break;
+            default:
+                    puntaje = "Deuce";
+                break;
+            
+        }
+    	return puntaje;
+    }
+    
+    private String GetEstadoMarcador() {
+    	
+    	   int RestaPuntaje = PuntajeJugador1 - PuntajeJugador2;
+           if (RestaPuntaje==1) 
+        	   puntaje ="Ventaja jugador1";
+           else if (RestaPuntaje ==-1)
+        	   puntaje ="Ventaja jugador2";
+           else if 	(RestaPuntaje>=2)
+        	   puntaje = "Gana jugador1";
+           else 
+        	   puntaje ="Gana jugador2";
+           
+    	return puntaje;
+    
+    }
+    
+    private String GetResultados() {
+        for (int i=1; i<3; i++)
+        {
+            if (i==1)
             {
-                if (i==1) tempPuntaje = m_puntaje1;
-                else { puntaje+="-"; tempPuntaje = m_puntaje2;}
-                switch(tempPuntaje)
-                {
-                    case 0:
-                        puntaje+="Amor";
-                        break;
-                    case 1:
-                        puntaje+="Quince";
-                        break;
-                    case 2:
-                        puntaje+="Treinta";
-                        break;
-                    case 3:
-                        puntaje+="Cuarenta";
-                        break;
-                }
+            	PuntajeTemporal = PuntajeJugador1;
+            }
+            else
+            {
+            	puntaje+="-"; 
+            	PuntajeTemporal = PuntajeJugador2;
+            }
+            switch(PuntajeTemporal)
+            {
+                case 0:
+                    puntaje+="Amor";
+                    break;
+                case 1:
+                    puntaje+="Quince";
+                    break;
+                case 2:
+                    puntaje+="Treinta";
+                    break;
+                case 3:
+                    puntaje+="Cuarenta";
+                    break;
             }
         }
         return puntaje;
